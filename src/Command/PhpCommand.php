6841a7ed8386053a5d6b296c69e52a6b122df633
Arnout Boks <arnoutboks@gmail.com>
2018-05-14 20:24:47 +0200

    Add support for 'php -i' in php subcommand

--- src/Command/PhpCommand.php
+++ src/Command/PhpCommand.php
@@ -6,6 +6,7 @@ use Aboks\PhpSrcDevtools\DockerImage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class PhpCommand extends Command
@@ -16,13 +17,20 @@ class PhpCommand extends Command
         $this->setName("php");
         $this->setDescription("Runs the PHP CLI built from source");
 
-        $this->addArgument("args", InputArgument::IS_ARRAY | InputArgument::REQUIRED, "arguments to pass to the PHP CLI");
+        $this->addOption("info", "-i", InputOption::VALUE_NONE, "show PHP information");
+
+        $this->addArgument("args", InputArgument::IS_ARRAY, "arguments to pass to the PHP CLI");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $arguments = $input->getArgument('args');
+        if ($input->hasOption('info')) {
+            $arguments[] = '-i';
+        }
+
         $dockerImage = new DockerImage();
-        $process = $dockerImage->createProcess('run-cli', $input->getArgument('args'));
+        $process = $dockerImage->createProcess('run-cli', $arguments);
         $process->setTimeout(null);
 
         return ConsoleProcessBridge::runProcessFromConsole($process, $input, $output);
